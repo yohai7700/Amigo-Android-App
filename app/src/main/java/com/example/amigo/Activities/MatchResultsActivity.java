2afd039b66eb4e6a048680641879567b3d9382da
yohai <yohai7700@gmail.com>
2020-05-20 16:10:56 +0300

    commit check

--- app/src/main/java/com/example/amigo/Activities/MatchResultsActivity.java
+++ app/src/main/java/com/example/amigo/Activities/MatchResultsActivity.java
@@ -2,7 +2,6 @@ package com.example.amigo.Activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
